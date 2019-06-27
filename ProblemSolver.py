0d8b4ec28a57122034221479d163d2b802376100
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-06-27 15:13:03 -0300

    Create a window with label

---
+++ ProblemSolver.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+
+Large_font = ('Verdana', 12)
+
+
+class Solver(tk.Tk):
+
+    def __init__(self):
+
+        tk.Tk.__init__(self)
+        container = tk.Frame()
+
+        container.pack(side='top', fill='both', expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, sticky='nsew')
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text='Start Page', font=Large_font)
+        label.pack(pady=10, padx=10)
+
+
+app = Solver()
+app.mainloop()
+
+
+
